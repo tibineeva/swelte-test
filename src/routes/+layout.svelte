8d45283eb91c46778d22508d78f1df1b7e3666ba
tibineeva <tibineeva44@gmail.com>
2024-03-14 14:32:31 +0500

    add main page and nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,68 @@
 <nav>
     <div class="nav-container">
-        <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+        <a href="/" class="nav-logo" title="Back to Homepage">Gallery Name</a>
         <div class="nav-links">
-            <a href="/about" class="link">Обо мне</a>
-            <a href="/blog" class="link">Блог</a>
-            <a href="/projects" class="link">Портфолио</a>
-            <a href="/contact" class="link">Контакты</a>
+            {#each nav as link}
+                <a href={link.href} class="link">{link.title}</a>
+            {/each}
         </div>
     </div>
 </nav>
 <div class="container">
-    <!-- Pages will be injected below -->
+    <!-- Images will be displayed below -->
     <slot />
 </div>
+<script>
+    const nav = [
+        {
+            title: "Nature",
+            href: "/nature",
+        },
+        {
+            title: "Animals",
+            href: "/animals",
+        },
+        {
+            title: "Architecture",
+            href: "/architecture",
+        },
+        {
+            title: "People",
+            href: "/people",
+        },
+        {
+            title: "temp",
+            href: "/temp",
+        },
+    ];
+</script>
 
 <style>
     .container {
         max-width: 1400px;
         margin: 50px auto;
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        gap: 20px;
+    }
+    nav {
+        background-color: #f3f3f3;
+        padding: 10px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    }
+    .nav-logo {
+        font-size: 24px;
+        font-weight: bold;
+        text-decoration: none;
+        color: #333;
+    }
+    .nav-links {
+        display: flex;
+        justify-content: flex-end;
+    }
+    .link {
+        margin-left: 10px;
+        color: #333;
+        text-decoration: none;
     }
 </style>
