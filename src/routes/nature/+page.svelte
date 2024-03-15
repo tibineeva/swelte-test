84a538b8546754004afaacf991f43c3ee9a791ed
tibineeva <tibineeva44@gmail.com>
2024-03-15 19:01:59 +0500

    add pages

--- src/routes/nature/+page.svelte
+++ src/routes/nature/+page.svelte
@@ -1,6 +1,23 @@
 <!-- Nature.svelte -->
 <script>
-    let images = ['nature1.jpg', 'nature2.jpg', 'nature3.jpg'];
+    let images = [
+        {
+            src: 'https://art-dot.ru/wp-content/uploads/2020/10/klod-mone-rybatskii-domik-v-varanzh-vile.jpg',
+            alt: 'Рыбацкий домик в Варанж-Виле',
+            caption: 'Клод Моне]'
+        },
+        {
+            src: 'https://art-dot.ru/wp-content/uploads/2020/10/ivan-shishkin-utro-v-sosnovom-lesu.jpg',
+            alt: 'Утро в сосновом лесу',
+            caption: 'Иван Шишкин'
+        },
+        {
+            src: 'https://art-dot.ru/wp-content/uploads/2020/10/andrei-martynov-narodnye-gulyaniya-v-dvortsovom-parke-pod-sankt-peterburgom.jpg',
+            alt: 'Народные гуляния в дворцовом парке под Санкт-Петербургом',
+            caption: 'Андрей Мартынов'
+        }
+    ];
+
     let currentImage = 0;
 
     function nextImage() {
@@ -12,10 +29,14 @@
     }
 </script>
 
-<h1>Nature</h1>
+<h1>Nature Gallery</h1>
 
 <div class="gallery">
-    <img src={images[currentImage]} alt="Nature Image" />
+    <a href={images[currentImage].src}>
+        <img src={images[currentImage].src} alt={images[currentImage].alt} />
+    </a>
+    
+    <p>{images[currentImage].caption}</p>
 
     <div class="navigation">
         <button on:click={prevImage}>Previous</button>
